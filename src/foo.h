679006626464f09d63947817fadcd821ae328c30
Michael Davidsaver <mdavidsaver@gmail.com>
2025-05-25 17:48:40 -0700

    update

--- src/foo.h
+++ src/foo.h
@@ -5,21 +5,29 @@
 extern "C" {
 #endif
 
-#ifndef _WIN32
+#if defined(_WIN32)
+
+#  define _CCALL __cdecl
+#  define _STDCALL __stdcall
+
+#  ifdef BUILDING_FOO
+#    define FOO_EXPORT __declspec(dllexport)
+#  else
+#    define FOO_EXPORT __declspec(dllimport)
+#  endif
+
+#elif __GNUC__ >= 4
+
+#  define _CCALL
+#  define _STDCALL
+#  define FOO_EXPORT __attribute__ ((visibility("default")))
+
+#else
+
 #  define _CCALL
 #  define _STDCALL
 #  define FOO_EXPORT
 
-#else
-#  define _CCALL __cdecl
-#  define _STDCALL __stdcall
-
-#  ifdef foo_EXPORTS
-#    define FOO_EXPORT __declspec(dllexport)
-
-#  else
-#    define FOO_EXPORT __declspec(dllimport)
-#  endif
 #endif
 
 FOO_EXPORT int _CCALL fn1(int);
